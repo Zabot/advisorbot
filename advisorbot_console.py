3d1a7d0a65c0d6d9be169820bfe9b9089b79b13c
Zach Anderson <zach.inbox@gmail.com>
2017-03-10 12:57:35 -0600

    Modified advisorbot_console to be input redirect friendly

--- advisorbot_console.py
+++ advisorbot_console.py
@@ -3,8 +3,10 @@
 import sys
 import question_handler
 
-while not False:
-    line = sys.stdin.readline()
+print("Any text on stdin while be interpreted as if it was in a message to advisorbot")
+print("Ctrl-D to exit")
+
+for line in sys.stdin:
     response = question_handler.handle(line)
     print(response)
 
